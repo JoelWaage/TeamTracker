5970e1130b382b4619e593101852c70a0aff755e
Joel <joeltwaage@gmail.com>
2016-09-20 15:49:41 -0700

    Create Delete Function

--- src/TeamTracker/Controllers/TeamsController.cs
+++ src/TeamTracker/Controllers/TeamsController.cs
@@ -49,5 +49,20 @@ namespace TeamTracker.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult Delete(int id)
+        {
+            var thisTeam = db.Teams.FirstOrDefault(teams => teams.TeamId == id);
+            return View(thisTeam);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisTeam = db.Teams.FirstOrDefault(teams => teams.TeamId == id);
+            db.Teams.Remove(thisTeam);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
